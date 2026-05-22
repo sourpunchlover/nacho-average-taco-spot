7db050bfb37bc8a719d20a5a209409054c5de5bf
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-22 16:21:44 -0400

    Created Order & Taco classes. Added initial code to Order.

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
 public class MainApp {
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ src/main/java/com/pluralsight/Order.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Taco> tacos;
+    private ArrayList<Drink> drinks;
+    private ArrayList<ChipsAndSalsa> chips;
+
+    public Order() {// no parameters b/c order is starting from 0
+        this.tacos = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
+    }
+    public double getTotal () {
+        return tacos.stream()
+                .mapToDouble(taco-> taco.getPrice())
+                .sum() +
+        drinks.stream()
+                .mapToDouble(drink-> drink.getPrice())
+                .sum() +
+        chips.stream()
+                .mapToDouble(chips-> chips.getPrice())
+                .sum();
+    }
+    public String getOrderSummary () {
+        return "";
+    }
+    public void addTaco (Taco taco){
+        this.tacos.add(taco);
+    }
+    public void addDrink (Drink drink){
+        this.drinks.add(drink);
+    }
+    public void addChips (ChipsAndSalsa chips){
+        this.chips.add(chips);
+    }
+
+}
