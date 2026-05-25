c8a28af59499aea379f06b86907a94734514409c
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-25 16:18:10 -0400

    Added full code to Topping & made it abstract. Added full code to PremiumTopping. Added full code to RegularTopping.

--- src/main/java/com/pluralsight/PremiumTopping.java
+++ src/main/java/com/pluralsight/PremiumTopping.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
-public class PremiumTopping {
+public class PremiumTopping extends Topping {
+    private double singlePrice, platePrice, burritoPrice;
+
+    //constructor
+    public PremiumTopping(String name, double singlePrice, double platePrice, double burritoPrice) {
+        super(name);
+        this.singlePrice = singlePrice;
+        this.platePrice = platePrice;
+        this.burritoPrice = burritoPrice;
+    }
+
+    @Override
+    public double getPrice(String size) {
+
+        switch (size) {
+            case "single":
+                return singlePrice;
+            case "3 taco plate":
+                return platePrice;
+            case "burrito":
+                return burritoPrice;
+            default:
+                System.out.println("Invalid option");
+                return 0;
+        }
+
+    }
+
+    public String toString(String size) {
+        String topping = "Name: " + name + "\n" + "Extra: " + isExtra + "\n" + "Price: " + getPrice(size);
+        return topping;
+    }
 }
--- src/main/java/com/pluralsight/RegularTopping.java
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
-public class RegularTopping {
+public class RegularTopping extends Topping {
+
+    public RegularTopping(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getPrice(String size) {
+        return 0;
+    }
+
+    public String toString(String size) {
+        String topping = "Name: " + name + "\n" + "Price: " + getPrice(size);
+        return topping;
+    }
+
 }
