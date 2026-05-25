45de17fb853735cfebfd8bb217e51d9db977bf83
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-25 16:44:55 -0400

    Added full code to Drink. Added full code to ChipsAndSalsa.

--- src/main/java/com/pluralsight/ChipsAndSalsa.java
+++ src/main/java/com/pluralsight/ChipsAndSalsa.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class ChipsAndSalsa {
+
+    public double getPrice() {
+        return 1.50;
+    }
+
+    public String toString() {
+        String addOn = "Chips and Salsa";
+        return addOn;
+
+    }
 }
--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
 public class Drink {
+    private String size;
+
+    //constructor
+
+    public Drink(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        double drinkPrice = 0;
+        switch (size) {
+            case "small":
+                drinkPrice = 2.00;
+                break;
+            case "medium":
+                drinkPrice = 2.50;
+                break;
+            case "large":
+                drinkPrice = 3.00;
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+        return drinkPrice;
+    }
+
+    public String toString() {
+        String drinkOrder = "Size: " + size + "\n";
+        return drinkOrder;
+
+    }
 }
