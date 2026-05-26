89c33adba774d9aaba4beae9449f37c687632924
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-25 21:44:01 -0400

    Created orderreceipts file. Added full code to Receipt.

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Receipt {
+    private String folderPath = "orderreceipts/";
+
+    public void saveOrderReceipt(Order order) {
+
+        LocalDateTime date = LocalDateTime.now();//gets current date
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String fileName = date.format(dateFormatter) + ".txt";
+
+
+        try {
+            FileWriter fileWriter = new FileWriter(folderPath + fileName);
+            BufferedWriter buffWriter = new BufferedWriter(fileWriter);
+            buffWriter.write(order.getOrderSummary());
+
+            buffWriter.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("Error! Receipt not saved!");
+        }
+    }
 }
