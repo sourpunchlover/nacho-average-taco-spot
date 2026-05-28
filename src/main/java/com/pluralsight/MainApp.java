4f617786c077a2223d8c0d9eabe63a172d30ca0f
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-27 21:34:01 -0400

    Updated code in MainApp to ui.displayHomeScreen()

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -3,6 +3,6 @@ package com.pluralsight;
 public class MainApp {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.display();
+        ui.displayHomeScreen();
     }
 }
